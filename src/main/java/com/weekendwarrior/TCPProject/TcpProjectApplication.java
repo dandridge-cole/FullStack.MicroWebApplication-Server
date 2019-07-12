816a07968b537056565aaf5495ff008a98b18060
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-07-12 12:54:24 -0400

    Add

--- src/main/java/com/weekendwarrior/TCPProject/TcpProjectApplication.java
+++ src/main/java/com/weekendwarrior/TCPProject/TcpProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.TCPProject;
+package com.weekendwarrior.TCPProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
