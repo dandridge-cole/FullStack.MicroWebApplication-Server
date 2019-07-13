f1107950199f79b9d1d6076f9d6fcfa48e00e2d0
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-13 08:31:31 -0400

    stubbing out User model, repo, service & controller

---
+++ src/main/java/com/weekendwarrior/TCPProject/Models/User.java
@@ -0,0 +1,45 @@
+package com.weekendwarrior.TCPProject.Models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String userName;
+    private Boolean authenticated;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Boolean getAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAuthenticated(Boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+}
