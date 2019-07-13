6345909561ddb55cf8fb44aba6fa4d3fb71163c3
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-07-13 14:23:02 -0400

    upload

--- src/main/java/com/weekendwarrior/TCPProject/Controllers/MessageController.java
+++ src/main/java/com/weekendwarrior/TCPProject/Controllers/MessageController.java
@@ -1,4 +1,32 @@
 package com.weekendwarrior.TCPProject.Controllers;
 
+import com.weekendwarrior.TCPProject.Models.Message;
+import com.weekendwarrior.TCPProject.Services.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    MessageController(MessageService messageService){
+        this.messageService=messageService;
+    }
+
+
+    @GetMapping("/messages/{channelId")
+    public List<Message> getMessagesByChannelId(Integer channelId){
+        return messageService.getMessageByChannel(channelId);
+    }
+
+    @PostMapping("/messages")
+    public Message postMessage(@RequestBody Message message){
+        return messageService.postMessage(message);
+    }
+
+
 }
