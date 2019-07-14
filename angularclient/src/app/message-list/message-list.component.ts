80285d6614ea5cfd7a2c598223d3bca1b7db96ea
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-07-14 08:27:49 -0400

    added message list to git

---
+++ angularclient/src/app/message-list/message-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Message} from "../message";
+import { MessageService} from "../messageService";
+
+@Component({
+  selector: 'app-message-list',
+  templateUrl: './message-list.component.html',
+  styleUrls: ['./message-list.component.css']
+})
+export class MessageListComponent implements OnInit {
+
+  messages: Message[];
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit() {
+  this.messageService.findAll(1).subscribe(data => this.messages = data)
+  }
+
+
+
+
+}
